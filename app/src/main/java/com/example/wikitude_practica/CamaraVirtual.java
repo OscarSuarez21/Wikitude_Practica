f93c063db15ca8fb772493bb4608ce65f7805517
OscarSuarez21 <51452598+oscarsuarez21@users.noreply.github.com>
2021-03-23 11:14:00 -0500

    Nuevas acciones

---
+++ app/src/main/java/com/example/wikitude_practica/CamaraVirtual.java
@@ -0,0 +1,14 @@
+package com.example.wikitude_practica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CamaraVirtual extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camara_virtual);
+    }
+}
--- app/src/main/java/com/example/wikitude_practica/MainActivity.java
+++ app/src/main/java/com/example/wikitude_practica/MainActivity.java
@@ -2,13 +2,38 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
+
+    Button camara, info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        camara =(Button)findViewById(R.id.Camara);
+        info =(Button)findViewById(R.id.info);
+
+        camara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camara = new Intent(MainActivity.this, CamaraVirtual.class);
+                startActivity(camara);
+
+            }
+        });
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent info = new Intent(MainActivity.this, info.class);
+                startActivity(info);
+
+            }
+        });
     }
 }
